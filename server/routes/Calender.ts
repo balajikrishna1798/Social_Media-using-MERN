85da9ca8b8b10b040712d345c2a5530bc6078675
Balajikrishna <82527215+balajikrishna1798@users.noreply.github.com>
2022-10-02 22:55:06 +0530

    Delete server directory

--- server/routes/Calender.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createEvent, getEvent } from "../controller/calender";
-const router = express.Router();
-
-router.post("/createEvent",createEvent)
-router.get("/getEvent",getEvent)
-export default router;
